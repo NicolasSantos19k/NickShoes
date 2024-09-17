009f45c25461bf2bda108c65eb8502164fcd4868
NicolasSantos19k <144915645+NicolasSantos19k@users.noreply.github.com>
2024-09-17 18:25:37 -0300

    Update script.js

--- script.js
+++ script.js
@@ -26,3 +26,14 @@ function myFunction() {
       x.style.display = "block";
     }
   }
+
+  function showPwd(id, el) {
+    let x = document.getElementById(id);
+    if (x.type === "password") {
+      x.type = "text";
+      el.className = 'fa fa-eye-slash showpwd';
+    } else {
+      x.type = "password";
+      el.className = 'fa fa-eye showpwd';
+    }
+  }
